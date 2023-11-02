7a137ccfbe1bbedfcc1022558a51b941a4ebee41
De-Lynn <k.riz98@mail.ru>
2023-11-02 21:12:25 +0300

    details for advanced search added

--- src/components/AdvancedSearch.tsx
+++ src/components/AdvancedSearch.tsx
@@ -1,8 +1,16 @@
-import { Field, Form, Formik } from 'formik'
+import { Field, Form, Formik, FormikHelpers, useFormik, useFormikContext } from 'formik'
 import './AdvancedSearch.scss'
+import { useState } from 'react'
 
 function AdvancedSearch() {
-    const initialValues = {}
+    const initialValues = {
+        include: '',
+        exclude: '',
+        mealType: '',
+        dishType: '',
+        cuisineType: '',
+        health: '',
+    }
     const mealType = ['', 'Breakfast', 'Dinner', 'Lunch', 'Snack', 'Teatime']
     const dishType = [
         '', 'Biscuits and cookies', 'Bread', 'Cereals',
@@ -25,6 +33,9 @@ function AdvancedSearch() {
         'shellfish-free', 'soy-free', 'sugar-conscious', 'sulfite-free', 'tree-nut-free',
         'vegan', 'vegetarian', 'wheat-free'    
     ]
+
+    const [isDetailsShown, showDetails] = useState(false)
+
     return (
         <div className='advanced-search'>
             <div className="separator-container">
@@ -40,50 +51,94 @@ function AdvancedSearch() {
                     actions.setSubmitting(false);
                 }}
             >
-                <Form className='search-form'>
-                    <Field className="field select-field" id="mealType" name="mealType" 
-                        as="select"
-                    >
-                        {mealType.map(el => {
-                            return (
-                                el === ''
-                                ? <option value={el}>Any meal type</option>
-                                : <option value={el}>{el}</option>
-                            )
-                        })}
-                    </Field>
-                    <Field className="field select-field" id="dishType" name="dishType" as="select">
-                        {dishType.map(el => {
-                            return (
-                                el === ''
-                                ? <option value={el}>Any category</option>
-                                : <option value={el}>{el}</option>
-                            )
-                        })}
-                    </Field>
-                    <Field className="field select-field" id="cuisineType" name="cuisineType" 
-                        as="select"
-                    >
-                        {cuisineType.map(el => {
-                            return (
-                                el === ''
-                                ? <option value={el}>Any cuisine</option>
-                                : <option value={el}>{el}</option>
-                            )
-                        })}
-                    </Field>
-                    <Field className="field select-field" id="health" name="health" as="select">
-                        {health.map(el => {
-                            return (
-                                el === ''
-                                ? <option value={el}>Any diet</option>
-                                : <option value={el}>{el}</option>
-                            )
-                        })}
-                    </Field>
-                    <button className="field btn more-btn" type='button'>Ingredients, details</button>
-                    <button className="field btn submit-btn" type='submit'>Search for recipes</button>
-                </Form>
+                {(props) => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        dirty,
+                        isSubmitting,
+                        handleChange,
+                        handleBlur,
+                        resetForm
+                    } = props;
+                    return (
+                        <Form className='search-form'>
+                            <Field className="field select-field" id="mealType" name="mealType" 
+                                as="select"
+                            >
+                                {mealType.map(el => {
+                                    return (
+                                        el === ''
+                                        ? <option value={el}>Any meal type</option>
+                                        : <option value={el}>{el}</option>
+                                    )
+                                })}
+                            </Field>
+                            <Field className="field select-field" id="dishType" name="dishType" as="select">
+                                {dishType.map(el => {
+                                    return (
+                                        el === ''
+                                        ? <option value={el}>Any category</option>
+                                        : <option value={el}>{el}</option>
+                                    )
+                                })}
+                            </Field>
+                            <Field className="field select-field" id="cuisineType" name="cuisineType" 
+                                as="select"
+                            >
+                                {cuisineType.map(el => {
+                                    return (
+                                        el === ''
+                                        ? <option value={el}>Any cuisine</option>
+                                        : <option value={el}>{el}</option>
+                                    )
+                                })}
+                            </Field>
+                            <Field className="field select-field" id="health" name="health" as="select">
+                                {health.map(el => {
+                                    return (
+                                        el === ''
+                                        ? <option value={el}>Any diet</option>
+                                        : <option value={el}>{el}</option>
+                                    )
+                                })}
+                            </Field>
+                            <button className="field btn more-btn" type='button'
+                                onClick={() => showDetails(!isDetailsShown)}
+                            >
+                                Ingredients, details
+                            </button>
+                            <button className="field btn submit-btn" type='submit'>Search for recipes</button>
+                            {
+                            isDetailsShown && 
+                                <div className='details'>
+                                    <div className='h-line'></div>
+                                    <div className='ingredients'>
+                                        <div className='type include'>
+                                            <span className='title'>INCLUDE INGREDIENTS</span>
+                                            <Field className='input-field' id="include" name="include" placeholder="+ Ingredient">
+
+                                            </Field>
+                                        </div>
+                                        <div className='type exclude'>
+                                            <span className='title'>EXCLUDE INGREDIENTS</span>
+                                            <Field className='input-field' id="exclude" name="exclude" placeholder="- Ingredient">
+
+                                            </Field>
+                                        </div>
+                                    </div>
+                                    <button className='field btn reset-btn' type='button' 
+                                        onClick={() => resetForm()}
+                                    >
+                                       &#10005; Clear all
+                                    </button>
+                                </div>
+                            }
+                        </Form>
+                    )
+                }}
+                
             </Formik>
         </div>
     )
